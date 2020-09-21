b442d7e38009c865d9f8ea3faebd10635d83e57b
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-09-21 11:08:23 +0700

    Add files via upload

---
+++ tugas17.js
@@ -0,0 +1,12 @@
+var kalimat = "Saya beLajar bahaSa peMrograman deNgan khUsuk"
+
+console.log(kalimat.toLowerCase())
+console.log(kalimat.toUpperCase())
+
+var kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+console.log(kalimat2.toLowerCase())
+console.log(kalimat2.toUpperCase())
+
+var nilai = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+	console.log(nilai.charCodeAt(0),"-", nilai.charCodeAt(1),"-", nilai.charCodeAt(2), "-", nilai.charCodeAt(3),"-", nilai.charCodeAt(4),"-", nilai.charCodeAt(5),"-", nilai.charCodeAt(6))
